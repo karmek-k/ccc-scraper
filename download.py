2a3005b9670b2ac6caf75c1ca790f92bcb2c8531
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-08-15 12:44:01 +0200

    chore: clean up download.py

--- download.py
+++ download.py
@@ -4,16 +4,21 @@ from downloader.constants import get_toc_url, BASE_URL
 from downloader.resources import get_page
 
 
+def anchors_to_resources(anchors):
+    """Transform anchors from `soup.find_all('a') to chapter resource names"""
+
+    anchor_hrefs = map(lambda a: a['href'].replace('rkkk', 'kkk'), anchors)
+    return filter(lambda href: href.startswith('kkk'), anchor_hrefs)
+
+
 print('Downloading table of contents...')
 
 toc_page = get_page(get_toc_url())
 toc_soup = BeautifulSoup(toc_page, features='html.parser')
 
 anchors = toc_soup.find_all('a')
-anchor_hrefs = map(lambda a: a['href'].replace('rkkk', 'kkk'), anchors)
-chapter_resources = filter(lambda href: href.startswith('kkk'), anchor_hrefs)
 
-for resource in chapter_resources:
+for resource in anchors_to_resources(anchors):
     print(f'Downloading {resource}')
     get_page(f'{BASE_URL}/{resource}')
 
