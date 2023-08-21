d5e23335afe5bc375bee6c3fde026cec9d8a11b7
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-08-21 11:35:21 +0200

    chore(content): clean up code

--- download.py
+++ download.py
@@ -1,38 +1,7 @@
-import re
-
-from bs4 import BeautifulSoup
-
 from downloader.constants import get_toc_url, BASE_URL
 from downloader.resources import get_page
 from downloader.errors import RequestError
-
-
-def make_soup(html):
-    return BeautifulSoup(html, features='html.parser')
-
-
-def anchors_to_resources(anchors):
-    """Transform anchors from `soup.find_all('a') to chapter resource names"""
-
-    anchor_hrefs = map(lambda a: a['href'].replace('rkkk', 'kkk'), anchors)
-    return filter(lambda href: href.startswith('kkk'), anchor_hrefs)
-
-
-def find_footnote_resources(html):
-    """Finds footnote resources in a HTML document"""
-
-    soup = make_soup(html)
-    anchors = soup.find_all('a')
-
-    resources = map(lambda a: a.get('href', ''), anchors)
-
-    footnotes = set()
-    regex = r'^([^#]+)(#[0-9]+)?$'
-    for footnote in filter(lambda r: r.startswith('przypisy'), resources):
-        resource = re.search(regex, footnote).group(1)
-        footnotes.add(resource)
-    
-    return footnotes
+from downloader.content import make_soup, anchors_to_resources, find_footnote_resources
 
 
 def download(resource):
---
+++ downloader/content.py
@@ -0,0 +1,33 @@
+import re
+
+from bs4 import BeautifulSoup
+
+
+def make_soup(html):
+    """Creates a `BeautifulSoup` object"""
+
+    return BeautifulSoup(html, features='html.parser')
+
+
+def anchors_to_resources(anchors):
+    """Transform anchors from `soup.find_all('a') to chapter resource names"""
+
+    anchor_hrefs = map(lambda a: a['href'].replace('rkkk', 'kkk'), anchors)
+    return filter(lambda href: href.startswith('kkk'), anchor_hrefs)
+
+
+def find_footnote_resources(html):
+    """Finds footnote resources in a HTML document"""
+
+    soup = make_soup(html)
+    anchors = soup.find_all('a')
+
+    resources = map(lambda a: a.get('href', ''), anchors)
+
+    footnotes = set()
+    regex = r'^([^#]+)(#[0-9]+)?$'
+    for footnote in filter(lambda r: r.startswith('przypisy'), resources):
+        resource = re.search(regex, footnote).group(1)
+        footnotes.add(resource)
+    
+    return footnotes
