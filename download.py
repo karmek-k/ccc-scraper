bc329365d7eeba31d546a88a48f56f333370503d
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-08-21 11:30:10 +0200

    feat: download footnotes

--- download.py
+++ download.py
@@ -1,7 +1,14 @@
+import re
+
 from bs4 import BeautifulSoup
 
 from downloader.constants import get_toc_url, BASE_URL
 from downloader.resources import get_page
+from downloader.errors import RequestError
+
+
+def make_soup(html):
+    return BeautifulSoup(html, features='html.parser')
 
 
 def anchors_to_resources(anchors):
@@ -11,15 +18,46 @@ def anchors_to_resources(anchors):
     return filter(lambda href: href.startswith('kkk'), anchor_hrefs)
 
 
+def find_footnote_resources(html):
+    """Finds footnote resources in a HTML document"""
+
+    soup = make_soup(html)
+    anchors = soup.find_all('a')
+
+    resources = map(lambda a: a.get('href', ''), anchors)
+
+    footnotes = set()
+    regex = r'^([^#]+)(#[0-9]+)?$'
+    for footnote in filter(lambda r: r.startswith('przypisy'), resources):
+        resource = re.search(regex, footnote).group(1)
+        footnotes.add(resource)
+    
+    return footnotes
+
+
+def download(resource):
+    """Downloads a resource"""
+
+    print(f'Downloading {resource}')
+
+    try:
+        return get_page(f'{BASE_URL}/{resource}')
+    except RequestError as e:
+        print(f'Failed to download {resource} - status {e.response.status_code}')
+
+
 print('Downloading table of contents...')
 
 toc_page = get_page(get_toc_url())
-toc_soup = BeautifulSoup(toc_page, features='html.parser')
+toc_soup = make_soup(toc_page)
 
 anchors = toc_soup.find_all('a')
 
 for resource in anchors_to_resources(anchors):
-    print(f'Downloading {resource}')
-    get_page(f'{BASE_URL}/{resource}')
+    html = download(resource)
+
+    footnotes = find_footnote_resources(html)
+    for footnote in footnotes:
+        download(footnote)
 
 print('Done')
