00e0ae211f142cf2a19272ee99b24976b2bbc10a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-08-29 17:12:17 +0200

    feat(models): add `Part`

--- models/toc.py
+++ models/toc.py
@@ -1,7 +1,15 @@
-class Chapter:
-    """`Chapter` contains individual articles"""
+class Part:
+    """`Part` is a container for other `Part`s"""
+
+    def __init__(self, title, parts=None):
+        self.title = title
+        self.parts = parts
+
+
+class Chapter(Part):
+    """`Chapter` is a `Part` that contains individual articles"""
 
     def __init__(self, title, resource_name=None, article_range=None):
-        self.title = title
+        super().__init__(title, None)  # TODO: contains articles, not `None`
         self.resource_name = resource_name
         self.article_range = article_range
