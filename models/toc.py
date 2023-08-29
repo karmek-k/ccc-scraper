119abb7e2183ba7f092d835ea1d22319655ffbd5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-08-29 17:03:08 +0200

    fix(models): resource_name in Chapter is optional

--- models/toc.py
+++ models/toc.py
@@ -1,7 +1,7 @@
 class Chapter:
     """`Chapter` contains individual articles"""
 
-    def __init__(self, title, resource_name, article_range=None):
+    def __init__(self, title, resource_name=None, article_range=None):
         self.title = title
         self.resource_name = resource_name
         self.article_range = article_range
