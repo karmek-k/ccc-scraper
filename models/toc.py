404710511e58a968153aade02cb021b639dcea94
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-08-29 16:46:34 +0200

    feat(models): add `Chapter`

---
+++ models/toc.py
@@ -0,0 +1,7 @@
+class Chapter:
+    """`Chapter` contains individual articles"""
+
+    def __init__(self, title, resource_name, article_range=None):
+        self.title = title
+        self.resource_name = resource_name
+        self.article_range = article_range
